2d0cc74d3371e45001d6c1af009e4beaebcc4f02
Tudor Hotea <tudor.hotea@yahoo.com>
2020-05-09 19:38:33 +0300

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,32 @@ const config = {
   measurementId: "G-QJFWBQ41BX"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot) {
+    const { displayName, email } = userAuth;
+    const createAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating message', error.message);      
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
---
+++ src/test.js
@@ -0,0 +1,8 @@
+import firebase from 'firestore/app';
+import 'firebase/firestore';
+
+const firestore = firebase.firestore();
+
+firestore.collection('users').doc('zODeY8YocWFnzQUm22JI').collection('cartItems').doc('sUkFZtKM3d76NZ1NEr46');
+firestore.doc('/users/zODeY8YocWFnzQUm22JI/cartItems/sUkFZtKM3d76NZ1NEr46');
+firestore.collection('users/zODeY8YocWFnzQUm22JI/cartItems');
