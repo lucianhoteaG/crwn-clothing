1f1ade29b3d649838c9c867bd77f35a106c6d37e
Tudor Hotea <tudor.hotea@yahoo.com>
2020-05-12 09:12:28 +0300

    updated shop data with new date, updated cart utils to to group items in cart reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -7,11 +7,11 @@ import './collection-preview.styles.scss';
 const CollectionPreview = ({ title, items }) => (
   <div className='collection-preview'>
     <h1 className='title'>{title.toUpperCase()}</h1>
-    <div className='preview'>
+    <div clascName='preview'>
       {items
         .filter((item, idx) => idx < 4)
-        .map(({ id, ...otherItemProps }) => (
-          <CollectionItem key={id} { ...otherItemProps } />
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
       ))}
     </div>
   </div>
